d0dc1a3a4395a5245b07c94cac71c382c958bec4
Phoebe Orwell <phoebe.orwell@gmail.com>
2023-01-11 14:58:18 +0000

    Iteration1 complete

---
+++ models/Phone.model.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const phoneSchema = new Schema(
+  {
+    id: {
+      type: Number,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    manufacturer: {
+        type: String,
+      required: true
+    },
+    description: {
+        type: String,
+        required: true   
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    imageFileName: {
+        type: String,
+        required: true
+    },
+    screen: {
+        type: String,
+        required: true
+    },
+    processor: {
+        type: String,
+        required: true
+    },
+    ram: {
+        type: Number,
+        required: true
+    }
+  }
+);
+
+const Phone = model("Phone", phoneSchema);
+
+module.exports = Phone;
